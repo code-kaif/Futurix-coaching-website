bdb3bba801901127e289f97ee5d893c5e337f0ab
Kaif <kaif98747@gmail.com>
2025-02-26 21:26:41 +0530

    initail

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
-import Projects from "@/components/Projects";
-import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <div className="md:px-20 px-10">
       <Navbar />
       <Header />
-      {/*<About />
-      <Services />
-      <Projects />
-      <Contact />
-      <Footer /> */}
     </div>
   );
 }
